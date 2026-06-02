b5d0554cd2194c3763e15ed1394723e323d40aa8
Jamie Wood <jamie@cool.studio>
2026-06-03 00:19:50 +0100

    WIP

--- src/formats/Bitwarden.ts
+++ src/formats/Bitwarden.ts
@@ -135,6 +135,11 @@ export class Bitwarden extends AbstractFormat {
       });
     }
 
+    const userCollection = collectionStore.get(options.username);
+    if (credentialJson.collectionIds.length === 0 && userCollection) {
+      credentialJson.collectionIds.push(userCollection.id);
+    }
+
     let uris = [] as {
       uri: string;
     }[];
